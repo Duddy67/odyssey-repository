72711ba837fca508c4d5f9cb6872c7bb660730fd
Duddy67 <lucas.sanner@gmail.com>
2016-09-12 09:20:15 +0200

    Add template files

---
+++ templates/protostar/html/com_users/profile/default_core.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+?>
+
+<fieldset id="users-profile-core">
+	<legend>
+		<?php echo JText::_('COM_USERS_PROFILE_CORE_LEGEND'); ?>
+	</legend>
+	<dl class="dl-horizontal">
+	         <?php //Note: The core name field is moved into the default_custom template. ?>
+		<dt>
+			<?php echo JText::_('COM_USERS_PROFILE_USERNAME_LABEL'); ?>
+		</dt>
+		<dd>
+			<?php echo htmlspecialchars($this->data->username); ?>
+		</dd>
+		<dt>
+			<?php echo JText::_('COM_USERS_PROFILE_REGISTERED_DATE_LABEL'); ?>
+		</dt>
+		<dd>
+			<?php echo JHtml::_('date', $this->data->registerDate); ?>
+		</dd>
+		<dt>
+			<?php echo JText::_('COM_USERS_PROFILE_LAST_VISITED_DATE_LABEL'); ?>
+		</dt>
+
+		<?php if ($this->data->lastvisitDate != '0000-00-00 00:00:00'){?>
+			<dd>
+				<?php echo JHtml::_('date', $this->data->lastvisitDate); ?>
+			</dd>
+		<?php }
+		else
+		{?>
+			<dd>
+				<?php echo JText::_('COM_USERS_PROFILE_NEVER_VISITED'); ?>
+			</dd>
+		<?php } ?>
+
+	</dl>
+</fieldset>
