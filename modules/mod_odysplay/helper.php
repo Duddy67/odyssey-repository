49a6494a7413e44c978e899c7791ea810241cef6
Lucas <lucas.sanner@gmail.com>
2016-12-13 21:57:49 +0100

    Add some features to the Odysplay module.

--- modules/mod_odysplay/helper.php
+++ modules/mod_odysplay/helper.php
@@ -16,11 +16,17 @@ class ModOdysplayHelper {
     $nowDate = $db->quote(JFactory::getDate('now', JFactory::getConfig()->get('offset'))->toSql(true));
 
     $query->select('t.id,t.name,t.alias,t.intro_text,t.full_text,t.catid,'.
-		   't.published,t.checked_out,t.checked_out_time,t.created,'.
+		   't.published,t.travel_duration,t.created,'.
 		   't.created_by,t.access,t.params,t.metadata,t.metakey,t.metadesc,t.hits,'.
 		   't.publish_up,t.publish_down,t.language,t.modified,t.modified_by')
-	  ->from($db->qn('#__odyssey_travel').' AS t')
-	  ->where('t.access IN('.implode(',', $groups).')')
+	  ->from($db->qn('#__odyssey_travel').' AS t');
+
+    // Join on category table.
+    $query->select('ca.title AS category_title, ca.alias AS category_alias, ca.access AS category_access')
+	  ->join('LEFT', '#__categories AS ca on ca.id = t.catid');
+
+    $query->where('t.access IN('.implode(',', $groups).')')
+	  ->where('ca.access IN ('.implode(',', $groups).')')
 	  ->where('t.published=1')
 	  ->where('(t.publish_up = '.$nullDate.' OR t.publish_up <= '.$nowDate.')')
 	  ->where('(t.publish_down = '.$nullDate.' OR t.publish_down >= '.$nowDate.')')
@@ -61,14 +67,18 @@ class ModOdysplayHelper {
       $travel->image = 'modules/mod_odysplay/camera-icon.jpg';
 
       //Get the first image (if any) detected in the intro text.
-      if(preg_match('#<img.* src="(.+)"#iU', $travel->intro_text, $matches)) {
+      if($params->get('get_image_from') == 'introtext' && preg_match('#<img.* src="(.+)"#iU', $travel->intro_text, $matches)) {
 	$travel->image = $matches[1];
+      }
+
+      //Get the first image (if any) detected in the full text.
+      if($params->get('get_image_from') == 'fulltext' && preg_match('#<img.* src="(.+)"#iU', $travel->full_text, $matches)) {
+	$travel->image = $matches[1];
+      }
+
+      if($params->get('show_introtext')) {
 	//Remove all images from the intro text.
 	$travel->intro_text = preg_replace('#<img .+>#iU', '', $travel->intro_text);
-      }
-      //Get the first image (if any) detected in the full text.
-      elseif(preg_match('#<img.* src="(.+)"#iU', $travel->full_text, $matches)) {
-	$travel->image = $matches[1];
       }
 
       //Get the image width and height then retrieve the new image size according to the
@@ -84,6 +94,10 @@ class ModOdysplayHelper {
 	  $travel->price_starting_at = $priceStartingAt;
 	}
       }
+
+      // Get the tags
+      $travel->tags = new JHelperTags;
+      $travel->tags->getItemTags('com_odyssey.travel', $travel->id);
     }
 
     //Get possible price rules.
@@ -120,6 +134,16 @@ class ModOdysplayHelper {
     }   
 
     return $size;
+  }
+
+
+  public static function getMaxCharacters($text, $limit)
+  {
+    if(strlen($text) <= $limit) {
+      return $text;
+    }
+
+    return substr($text, 0, $limit).'...';
   }
 }
 
--- modules/mod_odysplay/tmpl/default.php
+++ modules/mod_odysplay/tmpl/default.php
@@ -23,32 +23,52 @@ $css->addStyleSheet(JURI::base().'modules/mod_odysplay/odysplay.css');
 
 <?php foreach($travels as $travel) : 
 	$link = JRoute::_(OdysseyHelperRoute::getTravelRoute($travel->slug, $travel->catid, $travel->language));
+	//var_dump($travel->tags);
 ?>
   <div class="odysplay-travel">
-    <a href="<?php echo $link; ?>"><h2 class="travel-name"><?php echo $travel->name; ?></h2></a>
+    <?php if($params->get('show_name')) : ?>
+      <?php if($params->get('linked_name')) : ?>
+	<a href="<?php echo $link; ?>">
+      <?php endif; ?>
+	<h2 class="travel-name"><?php echo ModOdysplayHelper::getMaxCharacters($travel->name, $params->get('name_max_char')); ?></h2>
+      <?php if($params->get('linked_name')) : ?>
+	</a>
+      <?php endif; ?>
+    <?php endif; ?>
 
-    <?php if($params->get('show_image')) : ?>
+    <?php if($params->get('show_image')) :
+	    if($params->get('link_image')) : ?>
       <a href="<?php echo $link; ?>">
+	  <?php endif; ?>
 	<img src="<?php echo $travel->image; ?>" width="<?php echo $travel->img_width; ?>"
-	     height="<?php echo $travel->img_height; ?>" class="travel-image" /></a>
+	     height="<?php echo $travel->img_height; ?>" class="travel-image" />
+	<?php if($params->get('link_image')) : ?>
+	  </a>
+	<?php endif; ?>
     <?php endif; ?>
 
     <?php if($params->get('show_introtext')) : ?>
-      <div class="travel-text"><?php echo $travel->intro_text; ?></div>
+      <div class="travel-text"><?php echo ModOdysplayHelper::getMaxCharacters($travel->intro_text, $params->get('text_max_char')); ?></div>
     <?php endif; ?>
 
-    <div class="travel-price">
-      <span class="item-field"><?php echo JText::_('MOD_ODYSPLAY_PRICE_STARTING_AT'); ?></span>
-      <?php if(isset($travel->price_starting_at_prules) && $travel->price_starting_at_prules['price'] < $travel->price_starting_at) : ?>
-	<span class="normal-price">
-	  <?php echo UtilityHelper::formatNumber($travel->price_starting_at_prules['normal_price'], $digitsPrecision); ?></span>
-	<span class="price-starting-at">
-	  <?php echo UtilityHelper::formatNumber($travel->price_starting_at_prules['price'], $digitsPrecision); ?></span>
-      <?php else : ?>
-	<span class="price-starting-at"><?php echo UtilityHelper::formatNumber($travel->price_starting_at, $digitsPrecision); ?></span>
-      <?php endif; ?>
-      <span class="currency"><?php echo $currency; ?></span>
-    </div>
+    <?php if($params->get('show_travel_duration')) : ?>
+      <div class="travel-duration"><?php echo JText::_('MOD_ODYSPLAY_OPTION_TRAVEL_DURATION_'.$travel->travel_duration); ?></div>
+    <?php endif; ?>
+
+    <?php if($params->get('show_price')) : ?>
+      <div class="travel-price">
+	<span class="item-field"><?php echo JText::_('MOD_ODYSPLAY_PRICE_STARTING_AT'); ?></span>
+	<?php if(isset($travel->price_starting_at_prules) && $travel->price_starting_at_prules['price'] < $travel->price_starting_at) : ?>
+	  <span class="normal-price">
+	    <?php echo UtilityHelper::formatNumber($travel->price_starting_at_prules['normal_price'], $digitsPrecision); ?></span>
+	  <span class="price-starting-at">
+	    <?php echo UtilityHelper::formatNumber($travel->price_starting_at_prules['price'], $digitsPrecision); ?></span>
+	<?php else : ?>
+	  <span class="price-starting-at"><?php echo UtilityHelper::formatNumber($travel->price_starting_at, $digitsPrecision); ?></span>
+	<?php endif; ?>
+	<span class="currency"><?php echo $currency; ?></span>
+      </div>
+    <?php endif; ?>
   </div>
 <?php endforeach; ?>
 </div>
